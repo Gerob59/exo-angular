f2d9a37a87198e256a5fe2879e2a6914bf807a74
Robin Hotton <Robin.hotton@hotmail.com>
2023-01-10 15:04:58 +0100

    horloge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,7 @@ import { HorlogeMenuComponent } from './components/horloge-menu/horloge-menu.com
     HorlogeComponent,
     HorlogeMenuComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
